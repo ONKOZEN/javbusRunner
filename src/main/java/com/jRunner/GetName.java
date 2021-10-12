dad04e7c69eadc12b2a50312d1709fa8f0948391
ONKOZEN <33984451+ONKOZEN@users.noreply.github.com>
2021-10-12 14:47:00 +0800

    get new formed name by video number [fix bug]

--- src/main/java/com/jRunner/GetName.java
+++ src/main/java/com/jRunner/GetName.java
@@ -2,6 +2,8 @@ package com.jRunner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,28 +15,31 @@ import java.util.regex.Pattern;
 public class GetName {
     public static void main(String[] args) {
         GetName getName = new GetName();
-        String path = "H:\\备份 温浩然\\G\\test";
+        String path = "H:\\备份 温浩然\\G\\F盘";
         File file = new File(path);
         File[] files = file.listFiles();
-        Map<String, String> extMap = getName.getExtMap();
+        Map<String, String> extMap = getExtMap();
         for (File f : files
         ) {
-            if (!f.isDirectory() && getName.ifInExtMap(extMap, getName.getExt(f))) {
+            if (!f.isDirectory() && ifInExtMap(extMap, getExt(f)) && !f.getName().contains("FIXED") && !f.getName().contains("!")) {
                 String filename = f.getName();
                 System.out.println(filename);
-                String midname = getName.getMidNewName(getName.getUrl(getName.getVideoNumber(filename)));
-                String newName = getName.getFileNewName(midname, filename, f);
+                String midname = getMidNewName(getUrl(getVideoNumber(filename)));
+                String newName = getFileNewName(midname, filename, f);
                 System.out.println(newName);
+                changeName(path, f, newName);
+            } else {
+                continue;
             }
         }
     }
 
-    public String getVideoNumber(String name) {
+    public static String getVideoNumber(String name) {
         if (name == null) {
             return null;
         }
         String regexNo = "[\\d]{2,5}";
-        String regexName = "[a-zA-Z]{2,6}";
+        String regexName = "[a-zA-Z]{2,7}";
         Matcher m1 = Pattern.compile(regexName).matcher(name);
         Matcher m2 = Pattern.compile(regexNo).matcher(name);
         while (m1.find() && m2.find()) {
@@ -43,21 +48,21 @@ public class GetName {
         return null;
     }
 
-    public String getUrl(String videoNumber) {
+    public static String getUrl(String videoNumber) {
         if (videoNumber == null) {
             return null;
         }
         return "https://www.busjav.blog/" + videoNumber;
     }
 
-    public String getExt(File file) {
+    public static String getExt(File file) {
         if (file != null) {
             return file.getName().substring(file.getName().lastIndexOf(".") + 1);
         }
         return null;
     }
 
-    public boolean ifInExtMap(Map<String, String> extMap, String string) {
+    public static boolean ifInExtMap(Map<String, String> extMap, String string) {
         if (extMap == null || string == null) {
             return false;
         }
@@ -67,36 +72,55 @@ public class GetName {
         return false;
     }
 
-    public Map<String, String> getExtMap() {
+    public static Map<String, String> getExtMap() {
         Map<String, String> extMap = new HashMap<>();
         extMap.put("avi", "avi");
         extMap.put("mkv", "mkv");
         extMap.put("mp4", "mp4");
+        extMap.put("MP4", "MP4");
         extMap.put("wmv", "wmv");
         extMap.put("rmvb", "rmvb");
         return extMap;
     }
 
-    public String getMidNewName(String url) {
+    public static String getMidNewName(String url) {
         if (url == null) {
             return null;
         }
         try {
             Document document = Jsoup.connect(url).get();
             String s = document.title();
-            String[] split = s.split(" ");
-            String name = document.getElementsByClass("star-name").select("a").get(0).text() + " ";
+            String[] split = null;
+            String name = null;
+            if (s.contains(":")) {
+                String so = s.replace(':', ' ');
+                split = so.split(" ");
+            } else if (s.contains("・")) {
+                String so = s.replace('・', ' ');
+                split = so.split(" ");
+            } else if (s.contains("%")) {
+                String so = s.replace('%', ' ');
+                split = so.split(" ");
+            } else {
+                split = s.split(" ");
+            }
+
+            if (document.getElementsByClass("star-name").size() == 0) {
+                name = "暫無出演者資訊";
+            } else {
+                name = document.getElementsByClass("star-name").select("a").get(0).text() + " ";
+            }
             for (int i = 0; i < split.length - 4; i++) {
                 name = name + split[i] + " ";
             }
-            return name + split[split.length - 4];
+            return name + split[split.length - 4] + " [FIXED]";
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public String getFileNewName(String midName, String oldName, File file) {
+    public static String getFileNewName(String midName, String oldName, File file) {
         if (midName != null) {
             if (oldName.contains("ncensore")) {
                 midName += " uncensored";
@@ -113,8 +137,28 @@ public class GetName {
             if (oldName.contains("丝")) {
                 midName += " 丝";
             }
+            if (oldName.contains("群底")) {
+                midName += " 群底";
+            }
+
             return midName + "." + getExt(file);
         }
         return "!" + oldName;
     }
+
+    public static void changeName(String path, File file, String name) {
+        if (file != null && name != null) {
+            File newFile = new File(path + "\\" + name);
+            if (newFile.exists()) {
+                return;
+            }
+            if (file.renameTo(newFile)) {
+                System.out.println("已重命名");
+            } else {
+                System.out.println("Error");
+            }
+        }
+        return;
+    }
+
 }
--- src/main/java/com/jRunner/RunOkHttp.java
+++ src/main/java/com/jRunner/RunOkHttp.java
@@ -5,23 +5,40 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import java.io.File;
 import java.io.IOException;
 
 public class RunOkHttp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        String path = "H:\\备份 温浩然\\G\\test";
+        File file = new File(path);
+        File[] files = file.listFiles();
+        for (File f : files
+        ) {
+           f.renameTo(new File(path + "\\" + getNewName(f, "newName岛君！！！！！！！！")));
+        }
     }
-    public void runHttp(){
-                String url = "https://www.busfan.icu/IPX-749";
+
+    public void runHttp() {
+        String url = "https://www.busfan.icu/IPX-749";
         OkHttpClient okHttpClient = new OkHttpClient();
         Request request = new Request.Builder().url(url).build();
         Call call = okHttpClient.newCall(request);
         try {
             Response response = call.execute();
             System.out.println(response.body().string());
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println("----");
+    }
+
+    public static String getNewName(File file, String newPart) {
+        if (file != null && newPart != null) {
+            String s = newPart + file.getName();
+            return s;
+        }
+        return null;
     }
 }
